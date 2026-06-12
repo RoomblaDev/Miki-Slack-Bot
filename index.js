b9b72335bda2857bc7668403a45dbda495801758
RoomblaDev <tiktok.rajongok@gmail.com>
2026-06-12 18:14:40 +0200

    Fix of skill issue with some errors

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 
 const { App } = require("@slack/bolt");
 
-const { App } = new App({
+const app = new App({
   token: process.env.SLACK_BOT_TOKEN,
   appToken: process.env.SLACK_APP_TOKEN,
   socketMode: true
